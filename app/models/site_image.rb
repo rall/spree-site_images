0c73fbac109ccb74b5d54f07cd70d6a78e221611
Richard Allaway <richard.allaway@gmail.com>
2010-08-08 01:24:20 -0400

    fix table name; set styles in a lambda

--- app/models/site_image.rb
+++ app/models/site_image.rb
@@ -1,18 +1,14 @@
 class SiteImage < Image
-  set_table_name :images
-  
+  set_table_name :assets
+   
   has_attached_file :attachment, 
-                    :styles => sizes, 
+                    :styles => lambda {
+                      PAGE_POSITIONS.inject({}) do |memo, position|
+                      memo.merge(position.first.to_sym => position.last["size"])
+                      end.reverse_merge(:mini => '48x48>')
+                    }, 
                     :default_style => :page,
                     :url => "/assets/site/:id/:style/:basename.:extension",
                     :path => ":rails_root/public/assets/site/:id/:style/:basename.:extension"
-  
-  private
-
-  def sizes
-    PAGE_POSITIONS.inject({}) do |memo, position|
-      memo.merge(position.first.to_sym => position.last["size"])
-    end.reverse_merge(:mini => '48x48>')
-  end
   
 end
