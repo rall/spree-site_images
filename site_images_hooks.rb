f37aab3cd8c98d1a4b836233831886304491a72c
Richard Allaway <richard.allaway@gmail.com>
2010-08-08 15:28:38 -0400

    remove old mentions of :image

--- site_images_hooks.rb
+++ site_images_hooks.rb
@@ -1,6 +1,6 @@
 class SiteImagesHooks < Spree::ThemeSupport::HookListener
   insert_after :admin_tabs do
-    %(<%=  tab(:images, :label => 'site_images', :route => 'admin_site_images')  %>)
+    %(<%=  tab(:site_images, :label => 'site_images', :route => 'admin_site_images')  %>)
   end
 end
 
