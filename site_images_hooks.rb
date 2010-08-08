7889d1742f04708217589762531bdf7a7bd42eb9
Richard Allaway <richard.allaway@gmail.com>
2010-08-07 23:17:38 -0400

    hook class bad name

--- site_images_hooks.rb
+++ site_images_hooks.rb
@@ -1,4 +1,4 @@
-class SiteImageHooks < Spree::ThemeSupport::HookListener
+class SiteImagesHooks < Spree::ThemeSupport::HookListener
   insert_after :admin_tabs do
     %(<%=  tab(:images, :label => 'site_images', :route => 'admin_site_images')  %>)
   end
