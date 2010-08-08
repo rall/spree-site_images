232e8b0ce94ceb2d9cdd92137ee9839b3a223e7d
Richard Allaway <richard.allaway@gmail.com>
2010-08-07 20:19:41 -0400

    store file on S3

--- app/models/uploaded_file.rb
+++ app/models/uploaded_file.rb
@@ -1,8 +1,16 @@
 class UploadedFile < ActiveRecord::Base
   belongs_to :user
-  has_attachment :storage => :file_system
+  has_attachment :storage => "s3",
+                 :path => "assets/products/:id/:style/:basename.:extension",
+                 :s3_credentials => {
+                   :access_key_id => ENV['S3_KEY']  || HEROKU_AWS_S3['access_key_id'],
+                   :secret_access_key => ENV['S3_SECRET'] || HEROKU_AWS_S3['secret_access_key']
+                  },
+                  :bucket => ENV['S3_BUCKET'] || HEROKU_AWS_S3['bucket']
 
   named_scope :default_order, :order => 'created_at DESC'
+
+  validates_presence_of :filename
 
   cattr_reader :per_page
   @@per_page = 20
